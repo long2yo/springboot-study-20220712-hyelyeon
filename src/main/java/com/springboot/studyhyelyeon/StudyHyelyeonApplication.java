9684d068ebb6adf4486598659379101063c53be1
long2yo <long2yo@naver.com>
2022-07-13 17:04:58 +0900

    init commit

---
+++ src/main/java/com/springboot/studyhyelyeon/StudyHyelyeonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyhyelyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHyelyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHyelyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyhyelyeon/StudyHyelyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyhyelyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHyelyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
